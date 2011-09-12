97543075ba966af88ca1ff684fa768b92fea900a
antoshalee <antoshalee@gmail.com>
2011-09-12 16:42:26 +0800

    new files generated with rails 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GovnoRf::Application.config.secret_token = '1089c34363e0734c403531e0ebf33cb39ff2b5a06e0d61e193b7f799ad6ef2e9cd1d7eb9f80093b1a56751bb1cd894b1211abba5b5302f409da885052f33208d'
