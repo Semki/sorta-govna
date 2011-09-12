f80ac98513fa0e446ad55f5402abb38dfa6599cc
antoshalee <antoshalee@gmail.com>
2011-09-12 16:38:42 +0800

    delete all generated files with rails 3.0.9

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GovnoRf::Application.config.secret_token = 'bc088693e49ae461caf9c1fcb6a5cb95960cdfcfa63e294a59c6bf223a404a774441bd4b59c0886a7a7069c98e3ca2eccd44943b5ed59d08a3d2b43624b3d769'
