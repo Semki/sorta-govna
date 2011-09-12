07d6174add433540114813c7f92b9324a974f795
antoshalee <antoshalee@gmail.com>
2011-09-12 18:46:47 +0800

    new rails-3.1 syntax for migrations

--- db/migrate/20110912053926_create_shits.rb
+++ db/migrate/20110912053926_create_shits.rb
@@ -1,13 +1,8 @@
 class CreateShits < ActiveRecord::Migration
-  def self.up
-    create_table :shits do |t|
+  def change
+    create_table(:shits) do |t|
       t.string :name
       t.integer :total
-      t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :shits
   end
 end
