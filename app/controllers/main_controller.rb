6dc31c4f8387c649eb3439b62a43fefcd235c813
AntonZh <djzhan@list.ru>
2011-09-22 22:54:44 +0800

    link to shit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,16 +17,16 @@ class MainController < ApplicationController
     if name != ''
       flash[:last_shit] = name
       name_upcase = name.mb_chars.upcase.strip
-      @shit = Shit.find_by_name(name)
+      @shit = Shit.find_by_name(name_upcase)
       if @shit.nil?
         @shit = Shit.new do |s|
-          s.name = name
+          s.name = name_upcase
           s.total = 0
         end
       end
       @shit.total = @shit.total + 1
       @shit.save
-      flash[:last_shit_count] = @shit.total
+      flash[:last_shit] = @shit
     end
     redirect_to :action => 'index'
   end
