10916487567fedf94b984d20c326ace7471b7498
AntonZh <djzhan@list.ru>
2011-09-24 01:19:32 +0800

    better model code

--- app/models/shit.rb
+++ app/models/shit.rb
@@ -1,22 +1,15 @@
 class Shit < ActiveRecord::Base
   
   def self.shit_by_name(name)
-    if !name.blank?    
-      name_to_store = name.mb_chars.upcase.strip
-      shit = self.find_or_initialize_by_name(name_to_store)
-      shit.vote
-    end
+    self.find_or_initialize_by_name(name.mb_chars.upcase.strip).vote unless name.blank?      
   end
   
   def capitalized_name
     self.name.mb_chars.capitalize
   end
   
-  def self.shit_by_id(id)
-    if !id.blank?
-      shit = self.find(id)
-      shit.vote
-    end
+  def self.shit_by_id(id)    
+    self.find(id).vote unless id.blank?      
   end
   
   def vote
