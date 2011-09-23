2e85670359c654cce48a29d8491fe7d3b45a99e5
antoshalee <antoshalee@gmail.com>
2011-09-23 18:52:31 +0800

    refactoring

--- app/models/shit.rb
+++ app/models/shit.rb
@@ -1,25 +1,23 @@
 class Shit < ActiveRecord::Base
+  
   def self.shit_by_name(name)
-    if name != ''
+    if name != ''    
       name_to_store = name.mb_chars.upcase.strip
-      shit = self.find_by_name(name_to_store)
-      if (shit.nil?)
-        shit = self.new
-        shit.name = name_to_store
-        shit.total = 0      
-      end
-      shit.total += 1
-      shit.save
-      shit
+      shit = self.find_or_initialize_by_name(name_to_store)
+      shit.vote
     end
   end
   
   def self.shit_by_id(id)
     if id != ''
       shit = self.find(id)
-      shit.total = shit.total + 1
-      shit.save
-      shit
+      shit.vote
     end
+  end
+  
+  def vote
+    self.total = self.total + 1
+    self.save
+    self
   end
 end
