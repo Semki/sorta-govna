bcfed5d6dac6d314eaa11db6c4f480099dbf9298
Anton <antoshalee@gmail.com>
2011-09-28 23:14:25 +0800

    private method

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,7 +10,6 @@ class MainController < ApplicationController
     @topshits = Shit.topshit(10)        
   end
 	
- 
   def shit_it
     make_notice_about_shitting(Shit.shit_by_name(params[:name]))    
     respond_with("shit_it")
@@ -21,15 +20,17 @@ class MainController < ApplicationController
     redirect_to :action => 'index'
   end
   
-  def make_notice_about_shitting(shit)
-    flash[:notice] = t(:officially_shit, :name=>shit.capitalized_name, :count=>shit.total.to_s)
-  end
-  
   def compare
     if params[:name]
       @sorted_shits = Shit.with_names(params[:name].values).sort_by{|s| s.total}.reverse
       @do_compare = true
     end
+  end
+  
+  private 
+  
+  def make_notice_about_shitting(shit)
+    flash[:notice] = t(:officially_shit, :name=>shit.capitalized_name, :count=>shit.total.to_s)
   end
   
 end
