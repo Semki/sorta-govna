a1cbe5d7ad21e4087669116affbcbe9d48a92fe7
AntonZh <anton.zhavoronkov@gmail.com>
2011-10-03 16:45:20 +0800

    name with synonyms in shit page

--- app/helpers/shits_helper.rb
+++ app/helpers/shits_helper.rb
@@ -1,2 +1,13 @@
+#encoding: utf-8
 module ShitsHelper
+  def name_with_synonyms(shit)
+    unless shit.nil?
+      synonyms_string = shit.synonyms.collect{ |s| s.name }.reject{ |name| name == shit.name }.join(', ')
+      unless synonyms_string.blank?
+        t(:name_aka_synonyms, :name => shit.capitalized_name, :synonyms => synonyms_string)
+      else
+        shit.capitalized_name
+      end
+    end
+  end
 end
