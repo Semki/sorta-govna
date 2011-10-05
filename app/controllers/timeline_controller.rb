6c0b5ddbcf1fffc88b5f7d46544022c8a4c1695d
Dennis Kerner <denkern@gmail.com>
2011-10-05 23:23:21 +0800

    issue #29

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,24 +2,11 @@ class TimelineController < ApplicationController
   def index
     @timeline = Timeline.limit(100)
   end
-  def show_by_shit
-    
-    shit = Shit.find(params[:shit_id])
-    timeline = shit.timelines.order(:date)
-    datesline = timeline.map{|x| x.date}
-    totalline = timeline.map{|x| x.total}
-    
-    width = 600
-    barwidth = 15
-    header_height = 50
-    height = header_height + totalline.length * barwidth
-    str = width.to_s + 'x' + height.to_s
-    
-    #@img = Gchart.bar(:title => 'by days',  :labels => shit.timelines.map{|x| x.date}, :legend => shit.name, :data => shit.timelines.map{|x| x.total})
-    @img = Gchart.bar(:title => t(:timeline_by_shit), :orientation => 'horizontal', :bar_width_and_spacing => barwidth, 
-    :axis_with_labels => 'y,r', :axis_labels => [datesline, totalline.reverse], :legend => shit.name, :data => totalline, :size => str)
-    
-    @img
+  
+  def show_by_shit_by_week
+    weekindex = 0
+    weekindex = params[:week].to_f unless params[:week].nil?
+    @img = Timeline.show_by_shit_by_week(params[:shit_id], weekindex, t(:timeline_by_shit))
   end
   
   def weekrating
