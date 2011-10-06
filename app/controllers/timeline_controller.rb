609ec1593e3b3f0e0bbcd133e49cc8eab3a1f44f
Anton Zhavoronkov <djzhan@list.ru>
2011-10-06 12:18:02 +0800

    timelite title hotfix

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -4,12 +4,14 @@ class TimelineController < ApplicationController
   end
   
   def show_by_shit_by_week
+    @title = t(:page_shit_by_date)
     weekindex = 0
     weekindex = params[:week].to_f unless params[:week].nil?
     @img = Timeline.show_by_shit_by_week(params[:shit_id], weekindex, t(:timeline_by_shit))
   end
   
   def weekrating
+    @title = t(:page_week_rating)
     datefinish = Date.today
     datestart = datefinish - 6
     @timeline = Timeline.get_by_period(datestart, datefinish)
