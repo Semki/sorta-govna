978fc26d2c625c3554938cfe77b2dc7b46e4a682
Dennis Kerner <denkern@gmail.com>
2011-10-15 16:25:02 +0800

    Issue 45

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require jquery_ujs
 //= require_tree .
+
+// this is a new func
+		$(document).ready(function(){
+				
+				$("#addInputButton").click(function(){
+								var inputs = $(".inputClass");
+								var length = inputs.length;
+								var clone = inputs.first().clone();
+								clone.val("");
+								clone.attr("name", "name["+(length)+"]");
+								var div = $("#inputsCollection");
+								div.append(clone);
+				});
+ 
+		});
