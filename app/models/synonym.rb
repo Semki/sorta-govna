b52743df76bb8f341fe71db689b4ad4cd2728d46
Anton Lee <antoshalee@gmail.com>
2014-02-24 14:44:06 +0400

    Refactor uniqueness validation

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -5,32 +5,19 @@ class Synonym < ActiveRecord::Base
   after_update :update_concept_name
   after_destroy :update_concept_name
 
-  validates :name, :presence => true
+  validates :name, :presence => true, uniqueness: {scope: :shit_id}
   validates :shit_id, :presence => true, :numericality => true
   validates :relevance, :presence => true, :numericality => {:greater_than => 0, :less_than_or_equal_to => 4}
-  validate :has_no_doublicate
   scope :autocomplete_scope, order("relevance desc")
-  
-  
+
   def description
     "#{name} #{ '(' + shit.name + ')' unless relevant_enough? } — #{shit.description}"
   end
-  
+
   def relevant_enough?
     relevance >= 3
   end
-  
-  # валидация по наличию дубликата. Дубликатом считаем синоним с таким же именем и концептом.
-  def has_no_doublicate
-    if id.nil?
-      is_empty = Synonym.where("shit_id = ? and name = ? ", shit_id, name).limit(1).empty?
-    else
-      is_empty = Synonym.where("shit_id = ? and name = ? and id <> ? ", shit_id, name, id).limit(1).empty?
-    end
-    # уже есть синоним с таким именем и концептом!
-    errors.add(:full_message, "doublicate_synonym_error") unless is_empty
-  end
-  
+
   private
 
   # Обновление имени концепта. Вычисляется наиболее релевантный синоним
